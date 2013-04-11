cbfc340776b809ba0de01e528c6ff9626561cee2
Andrew Crites <explosion-pills@aysites.com>
2013-04-10 22:36:24 -0400

    Tests for Container.
    Creating simple tests for Container usage.
    
    * Updated test configuration to include constant to directory

--- test/config.php
+++ test/config.php
@@ -6,6 +6,8 @@
 error_reporting(-1);
 assert_options(ASSERT_ACTIVE, true);
 
+define('TESTROOT', dirname(__FILE__));
+
 require 'src/autoload.php';
 
 abstract class NaF_TestCase extends PHPUnit_Framework_TestCase {}
--- test/unit/ContainerTest.php
+++ test/unit/ContainerTest.php
@@ -6,5 +6,24 @@ class ContainerText extends NaF_TestCase
         $cnt = new di\Container();
         $this->assertTrue(true, "container created and message reached");
     }
+
+    public function testCreateSimpleObject()
+    {
+        $cnt = new di\Container();
+        include TESTROOT . '/help/ObjectCreatedByContainer.php';
+        $obj = $cnt->create('ObjectCreatedByContainer');
+
+        $this->assertTrue($obj instanceof ObjectCreatedByContainer);
+    }
+
+    public function testCreateNestedObject()
+    {
+        $cnt = new di\Container();
+        include TESTROOT . '/help/ObjectCreatedByContainer.php';
+        include TESTROOT . '/help/ObjectWithDependency.php';
+        $obj = $cnt->create('ObjectWithDependency');
+
+        $this->assertTrue($obj instanceof ObjectWithDependency);
+    }
 }
 ?>
