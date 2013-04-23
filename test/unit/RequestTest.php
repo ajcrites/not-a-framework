5b058cd1c8c0a7691fcbbd40fa9f44717d45a948
Andrew Crites <explosion-pills@aysites.com>
2013-04-23 00:20:40 -0400

    Request: getter/setter/trim tests
    Writing tests for all facets of the
    'trim' and magical getter/setter methods

--- test/unit/RequestTest.php
+++ test/unit/RequestTest.php
@@ -12,10 +12,23 @@ class RequestTest extends Naf_TestCase
         $this->assertEquals($request->getMethod(), 'METHOD');
     }
 
-    public function testSimpleTrim()
+    public function testTrim()
     {
         $request = new Naf\http\Request('', '');
         $this->assertEquals($request->trim('  spaces  '), 'spaces');
+        $this->assertEquals($request->trim(array('  spaces  ', array('  spaces  '))), array('spaces', array('spaces')));
+    }
+
+    public function testMagicGetterSetter()
+    {
+        $request = new Naf\http\Request('', '');
+        //headers has the special default empty array value
+        $this->assertEquals($request->headers, array());
+        $this->assertNull($request->nonextant);
+        $request->one = 1;
+        $this->assertEquals($request->one, 1);
+        $request->array = array('of values');
+        $this->assertEquals($request->array, array('of values'));
     }
 }
 ?>
